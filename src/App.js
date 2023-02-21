5c1ec8bf91980db5c5fd59b7706d788f899c3b8b
aitezazshah <aitezazshah123@gmail.com>
2023-02-21 22:43:00 +0500

    Add Navbar to Home Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import LoginScreen from './components/LoginScreen';
+import HomeScreen from './components/HomeScreen';
 import './App.css';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <LoginScreen/>
+      <Routes>
+       <Route path="/" element={<LoginScreen />} />
+      <Route path="/home" element={<HomeScreen />}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+function HomeScreen() {
+  return (
+    <div>
+        <NavBar/>
+      <p>Hello this is Home Screen</p>
+    </div>
+  )
+}
+
+export default HomeScreen
